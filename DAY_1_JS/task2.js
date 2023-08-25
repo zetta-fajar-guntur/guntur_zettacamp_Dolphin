8794be1eeb690e55085b13a734506f9545753c92
zettacamp-fajar-guntur <fajar.guntur@zettacamp.pro>
2023-08-26 01:31:42 +0700

    Materi Day 1 JS
    learning about Javascript

---
+++ DAY_1_JS/task2.js
@@ -0,0 +1,9 @@
+let favoriteBook = 'Komik Boruto';
+const myBook = 'Novel NKCTHI';
+
+favoriteBook = 'Popular Magazine';
+console.log('1. ' + favoriteBook);
+console.log('2. ' + myBook);
+
+let ccGroup = favoriteBook + ', ' + myBook;
+console.log('3. ' + ccGroup);
